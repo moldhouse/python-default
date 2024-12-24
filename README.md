0e990f6ebb793afcad49486eb3c25e4dc00febf8
Moritz Althaus <moritzalthaus@gmx.de>
2024-12-24 13:56:48 +0100

    feat: initial commit

---
+++ README.md
@@ -0,0 +1,15 @@
+# Python Default
+
+Ideas on how to configure and setup a Python project.
+
+## Getting Started
+
+```sh
+uv sync
+```
+
+Run the tests with
+
+```sh
+uv run pytest
+```
---
+++ tests/calculator_test.py
@@ -0,0 +1,17 @@
+from src.calculator import add
+
+
+def test_add_positive_numbers():
+    assert add(1, 2) == 3
+
+
+def test_add_negative_numbers():
+    assert add(-1, -2) == -3
+
+
+def test_add_mixed_numbers():
+    assert add(1, -2) == -1
+
+
+def test_add_zero():
+    assert add(0, 0) == 0
